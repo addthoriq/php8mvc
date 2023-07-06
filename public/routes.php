d2044d39a2c9685fbccc269e1a19ff0f90dde682
addthoriq <alvarro.thoriq@gmail.com>
2023-07-06 14:33:15 +0700

    View Template

--- public/routes.php
+++ public/routes.php
@@ -3,10 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Kang\Phpmvc\App\Router;
-use Kang\Phpmvc\Controller\AboutController;
 use Kang\Phpmvc\Controller\HomeController;
 
-Router::add('GET', '/', HomeController::class, 'index');
-Router::add('GET', '/about', AboutController::class, 'index');
+Router::add('GET', '/', HomeController::class, 'index', []);
 
 Router::run();
